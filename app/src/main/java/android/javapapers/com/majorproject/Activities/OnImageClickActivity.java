080ce8b46c08d5c55c03c0092d52f9228f9e5ec9
AviD <avi9800@gmail.com>
2020-10-26 00:06:37 +0530

    Initial MajorProject project

---
+++ app/src/main/java/android/javapapers/com/majorproject/Activities/OnImageClickActivity.java
@@ -0,0 +1,15 @@
+package android.javapapers.com.majorproject.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.javapapers.com.majorproject.R;
+import android.os.Bundle;
+
+public class OnImageClickActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_on_image_click);
+    }
+}
